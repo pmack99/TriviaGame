6f020e9f6b098a1cd0a8f42e82b6405c7e6de35f
PMack2 <Prestonmack@gmail.com>
2018-09-16 12:25:57 -0400

    initial

---
+++ assets/js/app.js
@@ -0,0 +1,182 @@
+var triviaQuestions = [{
+	question: "What team did Babe Ruth play for before joining the Boston Red Sox?",
+	answerList: ["NY Yankees", "Baltimore Orioles", "Tampa Bay Rays", "Boston Braves"],
+	answer: 1
+},{
+	question: "What team moved to D.C. to become the Washington Nationals in 2005?",
+	answerList: ["Montreal Expos", "Milwaukee Brewers", "Brooklyn Dodgers", "Kansas CIty Monarchs"],
+	answer: 0
+},{
+	question: "Who has played the most consecutive games of baseball, breaking Lou Gehrig's record on September 6, 1995?",
+	answerList: ["Dave Winfield", "Mark McGuire", "Barry Bonds", "Cal Ripken Jr."],
+	answer: 3
+},{
+	question: "What 1929 feat did Lee Richmond perform in a Major League Baseball first?",
+	answerList: ["Hit a grand slam", "Struck out 6 times in one game", "Pitched a perfect game", "Hit 4 home runs in one game"],
+	answer: 2
+},{
+	question: "Before playing in the majors, Jackie Robinson played with the minor league Montreal Royals, the top farm club of which organization?",
+	answerList: ["Kansas City Royals", "NY Yankees", "Boston RedSox", "Brooklyn Dodgers"],
+	answer: 3
+},{
+	question: "What up-and-coming star player saw his first World Series with the Yankees in 1936 as Babe Ruth's replacement?",
+	answerList: ["Lou Gehrig", "Ty Cobb", "Joe DiMaggio", "Mickey Mantle"],
+	answer: 2
+},{
+	question: "What is the oldest active ball park in Major League Baseball?",
+	answerList: ["Yankee Stadium", "Fenway Park", "Wrigley Field", "Marlins Park"],
+	answer: 1
+},{
+	question: "What player was accused of using too much pine tar on his bat on July 24, 1983, causing a game-winning home run to be nullified?",
+	answerList: ["Dick Howser", "Graig Nettles", "George Brett", "Goose Gossage"],
+	answer: 2
+},{
+	question: "What basketball great made the surprising decision to switch sports, signing a minor league baseball contract with the Chicago White Sox in 1994?",
+	answerList: ["Steph Curry", "Michael Jordan", "LeBron James", "Magic Johnson"],
+	answer: 1
+},{
+	question: "Which team hosted the infamous Disco Demolition Night promotional event on July 12, 1979?",
+	answerList: ["Seattle Mariners", "Philadelphia Phillies", "Cleveland Indians", "Chicago WhiteSox" ],
+	answer: 3
+},{
+	question: "Who is the only pitcher in Major League Baseball to win a World Series games in three different decades?",
+	answerList: ["Jim Palmer", "Nolan Ryan", "Greg Maddux", "Mariano Rivera"],
+	answer: 0
+},{
+	question: "In the mythology surrounding the Chicago Cubs, what curse is responsible for the team not reaching the World Series since 1945?",
+	answerList: ["The Curveball Curse", "The Curse of the Billy Goat", "Curse of the Bambino", "The Chicago Fire Curse"],
+	answer: 1
+},{
+	question: "1998 saw a race between the St. Louis Cardinals' Mark McGwire and the Chicago Cubs' Sammy Sosa to break which player's 61 home run record?",
+	answerList: ["Babe Ruth", "Barry Bonds", "Willie Mays", "Roger Maris"],
+	answer: 3
+},{
+	question: "What baseball great is known for such colorful sayings as 'It ain't over till it's over'?",
+	answerList: ["Yogi Berra", "Whitey Herzog", "Billy Martin", "Honus Wagner"],
+	answer: 0
+},{
+	question: "Who holds the highest career batting average of all time?",
+	answerList: ["Pete Rose", "Ichiro", "Ty Cobb", "Don Mattingly"],
+	answer: 2
+}];
+
+
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 5;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 500);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
